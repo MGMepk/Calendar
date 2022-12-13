a40727e9dbdc5612c9b2f8f83fd32ab4ad7a665a
Manuel García <manuelgarcia.mgm@gmail.com>
2022-12-13 19:32:25 +0100

    primer commit

---
+++ calendar.js
@@ -0,0 +1,25 @@
+function calculoHoras() {
+  let total = document.getElementById("total").value;
+  let semana = [];
+  semana[0] = document.getElementById("lunes").value;
+  semana[1] = document.getElementById("martes").value;
+  semana[2] = document.getElementById("miercoles").value;
+  semana[3] = document.getElementById("jueves").value;
+  semana[4] = document.getElementById("viernes").value;
+  semana[5] = document.getElementById("sabado").value;
+  semana[6] = document.getElementById("domingo").value;
+
+  let hoyEs = new Date();
+  let dias = 0;
+  let array = 0;
+  while (total > 0) {
+    total = total - semana[hoyEs.getDay() - 1 + array];
+    dias++;
+    array++;
+    if (array >= 6) {
+      array = 0 - hoyEs.getDay();
+    }
+  }
+
+  console.log("dias: " + dias);
+}
